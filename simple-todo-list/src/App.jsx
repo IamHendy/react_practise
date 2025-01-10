07ae0d21b6218a3d97888f883de5add34eb1ff7a
IamHendy <puchhendy@gmail.com>
2025-01-10 14:33:13 +0300

    tasks

---
+++ simple-todo-list/src/App.jsx
@@ -0,0 +1,15 @@
+import Todo from './components/Todo';
+import TodoItems from './components/TodoItems';
+
+const App = () => {
+ 
+  return(
+      <div>
+      <Todo/>
+      <TodoItems/>
+    </div>
+  )
+
+} 
+
+export default App
